9efb376f481e3288e96b4dc01f5ccecd94278cee
Dennis Kim <denniskim14@gmail.com>
2018-01-17 15:34:12 -0500

    oh baby a complete redesign

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ $( document ).ready(function(){
 
 	$("#linkone").click(function(){
 		$('html, body').animate({
-			scrollTop: $("#siteredesign").offset().top}, 2000);
+			scrollTop: $("#siteredesign").offset().top}, 400);
 	});
 
 	$("#linktwo").click(function(){
 		$('html, body').animate({
-			scrollTop: $("#statapp").offset().top}, 2000);
+			scrollTop: $("#statapp").offset().top}, 1400);
 	});
 
 	
