6e7c6a3ee85ed4dce5030ac829961084e7c9548f
Dennis Kim <denniskim14@gmail.com>
2019-12-04 17:00:46 -0500

    big push

--- main.js
+++ main.js
@@ -13,9 +13,19 @@ $( document ).ready(function(){
 	$("#finalproduct").click(function(){
 		$('html, body').animate({
 			scrollTop: $("#newsite").offset().top}, 1400);
-	});
+	});	
+});
 
-
-
-	
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      	
+      	if(objectBottom < windowBottom){
+      		$(this).addClass("fade-ani");
+      	} 
+    });
+  }).scroll(); //invoke scroll-handler on page-load
 });
