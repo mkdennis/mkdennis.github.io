99dc13a4a6dd42c3170c92bec8329fe3d2775a0a
Dennis Kim <denniskim14@gmail.com>
2019-12-10 01:32:54 -0500

    done with responsive i think

--- main.js
+++ main.js
@@ -18,10 +18,13 @@ $( document ).ready(function(){
 
 $(window).on("load",function() {
   $(window).scroll(function() {
+
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    console.log("Inner Height: " + windowBottom)
+
     $(".fade").each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      var objectBottom = $(this).offset().top;
       	
       	if(objectBottom < windowBottom){
       		$(this).addClass("fade-ani");
@@ -29,3 +32,8 @@ $(window).on("load",function() {
     });
   }).scroll(); //invoke scroll-handler on page-load
 });
+
+
+//when bottom of scroll hits top of object position - fade
+// if scroll.position > top of object.position {
+//	add fade }
