693dbadd30d4a23779fdb46d3f29971853cdc5eb
Dennis Kim <denniskim14@gmail.com>
2019-12-17 00:44:43 -0500

    Oh baby - we adulting

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ $(window).on("load",function() {
   $(window).scroll(function() {
 
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    console.log("Inner Height: " + windowBottom)
 
     $(".fade").each(function() {
       /* Check the location of each desired element */
@@ -32,6 +31,43 @@ $(window).on("load",function() {
     });
   }).scroll(); //invoke scroll-handler on page-load
 });
+
+//red to blue animation
+
+//just want red block to decrease height with scroll after certain point
+
+
+$(window).on("load",function() {
+	$(window).scroll(function() { //if scroll position changes, call function, not if passed certain point, 
+
+		//scroll top is y = 0, innerHeight is height of window
+		var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+		console.log("windowBottom: " + windowBottom);
+		console.log("redPosition: " + redPosition)
+		if(windowBottom > redPosition){blockgrow(windowBottom);}
+	});
+	
+
+	var redPosition = $(".red-block").offset().top + ($(".red-block").innerHeight());
+	var bluePosition = $(".blue-block").offset().top;
+	console.log("redPosition: " + redPosition)
+
+
+	function blockgrow(x){
+
+			$(".red-block").css("height", function(){
+				var a = parseInt($(this).css("height"),10);
+				return a - 1;
+			})
+			$(".blue-block").css("height", function(index){
+
+				var b = parseInt($(this).css("height"),10);
+				return b + 1;
+			})
+	}
+
+});
+
 
 
 //when bottom of scroll hits top of object position - fade
